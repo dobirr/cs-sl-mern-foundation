4d5164e6606209be8bba61d6cc2873ed37e7156b
Dennis Dobirr <dennis.dobirr@realcore.de>
2026-01-03 17:12:02 +0100

    feat(client/header): add lucid icons to navigation

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import './style.css';
 import { renderLoginPage, attachLoginHandlers } from './pages/login';
 import { renderRegisterPage, attachRegisterHandlers } from './pages/register.js';
 import { attachLandingHandlers, renderLandingPage } from './pages/landing.js';
+import { createIcons, icons } from 'lucide';
 
 const routes = {
   '/': { view: renderLandingPage, mount: attachLandingHandlers },
@@ -18,14 +19,18 @@ function render(pathname = window.location.pathname) {
   const html = view();
   document.querySelector('#root').innerHTML = `
     <div class="app-shell">
-      <header class="py-3 border-bottom bg-white shadow-sm">
-        <div class="container d-flex justify-content-between align-items-center">
+      <header class="py-3 bg-white">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
           <span class="brand">
             <a href="/" data-link class="text-decoration-none">HEALTHY Delights</a>
           </span>
-          <nav class="d-flex gap-3">            
-            <a href="/login" data-link class="text-decoration-none">Login</a>
-            <a href="/cart" data-link class="text-decoration-none">Cart</a>
+          <nav class="d-flex gap-4">            
+            <a class="login" href="/login" data-link class="text-decoration-none">
+              <i data-lucide="user"></i>
+            </a>
+            <a class="cart" href="/cart" data-link class="text-decoration-none d-flex align-items-center gap-1">
+              <i data-lucide="store"></i>
+            </a>
           </nav>
         </div>
       </header>
@@ -36,6 +41,7 @@ function render(pathname = window.location.pathname) {
   if (typeof mount === 'function') {
     mount();
   }
+  createIcons({ icons });
 }
 
 render();
